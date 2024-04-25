febd8bb5019f3924075aadbc6669e4cbc754c1bf
JiHyeunM <moon011011@gmail.com>
2024-04-25 23:08:29 +0900

    프로그래머스 코딩테스트/배열 뒤집기

---
+++ 배열뒤집기.js
@@ -0,0 +1,8 @@
+// 정수가 들어 있는 배열 num_list가 매개변수로 주어집니다.
+// num_list의 원소의 순서를 거꾸로 뒤집은 배열을 return하도록 solution 함수를 완성해주세요.
+
+function solution(num_list) {
+  num_list = [1, 0, 1, 1, 1, 3, 5];
+  return num_list.reverse();
+}
+console.log(solution());
